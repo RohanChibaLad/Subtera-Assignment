bc9cd44bf44d32a3f902d4c3ffa141901409f661
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-09-25 16:36:50 +0100

    Set the DB up

---
+++ backend/app/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./library.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}) #Makes sure its SQLite compatible
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #Creates a session factory
+
+Base = declarative_base() #Base class for our models
+
+def get_db():
+    """Dependency to get DB session"""
+    db = SessionLocal() #Creates a new session
+    try:
+        yield db #Yields the session to be used in the request
+    finally:
+        db.close() #Closes the session after the request is done
