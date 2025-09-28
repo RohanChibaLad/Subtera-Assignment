0053d468837b97645bb0be7762723ba8837ac662
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-09-28 19:26:58 +0100

    Added some validations to readers (that were missed) and finished reader tests

--- backend/app/routers/readers.py
+++ backend/app/routers/readers.py
@@ -39,10 +39,12 @@ def update_reader(reader_id: int, reader_update: schemas.ReaderUpdate, db: Sessi
     reader = db.query(models.Reader).filter(models.Reader.id == reader_id).first()
     if not reader:
         raise HTTPException(status_code=404, detail="Reader not found")
-    
+
     if reader_update.name is not None:
         reader.name = reader_update.name
     if reader_update.email is not None:
+        if db.get(models.Reader, reader_update.email):
+            raise HTTPException(status_code=400, detail="Email already registered.")
         reader.email = reader_update.email
     
     try:
