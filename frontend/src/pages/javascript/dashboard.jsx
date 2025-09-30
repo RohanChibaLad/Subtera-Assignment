51ae99ad97b9c4e68177aaa8e035139b80209052
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-09-30 14:42:16 +0100

    Javascript to load the data from back end implemented

--- frontend/src/pages/javascript/dashboard.jsx
+++ frontend/src/pages/javascript/dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { Stats } from "./api";
 
 export default function Dashboard() {
@@ -11,4 +11,48 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState("");
 
+  useEffect(() => {
+    // mounted variable will be used as a flag to prevent updates if the components unmounts mid request
+    let mounted = true;
+
+    (async () => {
+      setLoading(true);
+      setErr("");
+      
+      try {
+      // Get all the request in parallel and wait for them all to come back (or for one to throw an error)
+      const [pb, pa, ut, uta] = await Promise.all([
+        Stats.popularBooks(10),
+        Stats.popularAuthors(10),
+        Stats.userTotal(),
+        Stats.userTopAuthors(3)
+      ]);
+
+      // Avoid setting the state if unmounted
+      if (!mounted){
+        return;
+      }
+
+      // Set the states with the data we just retrieved
+      setPopularBooks(pb)
+      setPopularAuthors(pa)
+      setUserTotal(ut)
+      setUserTopAuthors(uta)
+
+    }
+    catch (e) {
+      if (!mounted) {
+        return;
+      }
+      // Error message
+      setErr(e.message || "Failed to load data")
+    }
+    finally {
+      if (mounted) {
+        // Finished loading the data
+        setLoading(false);
+        }
+      }
+    })
+  })
 }
