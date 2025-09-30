5a0f37bbbf44a7d6a73a238f039b84c7da26cdc8
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-09-30 15:35:12 +0100

    Finished simple css styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./pages/css/dashboard.css";
 import Dashboard from "./pages/javascript/dashboard";
 
 createRoot(document.getElementById("root")).render(
